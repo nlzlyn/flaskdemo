10174c74096e126b10ca9b1c88ed4f4664e4f10e
nlzly <nlzly@163.com>
2018-11-16 10:48:39 +0800

    Flask Web开发 1-7代码

---
+++ flasky/app/main/views.py
@@ -0,0 +1,29 @@
+from flask import render_template, session, redirect, url_for, flash, current_app
+from datetime import datetime
+from . import main
+from .forms import NameForm
+from ..models import User
+from ..email import send_email
+from .. import db
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+            flash('Welcome to join us.')
+            if current_app.config['FLASKY_ADMIN']:
+                send_email(current_app.config['FLASKY_ADMIN'], 'New User', 'mail/new_user', user=form.name.data)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('main.index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+        known=session.get('known', False), current_time=datetime.utcnow())
